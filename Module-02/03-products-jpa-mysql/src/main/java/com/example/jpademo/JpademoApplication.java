646ee5230a7a895f1b74c3b41294ac37c5bef540
Ashok S <ashok.s@koenig-solutions.com>
2025-07-22 10:34:50 +0530

    product rest api

---
+++ Module-02/03-products-jpa-mysql/src/main/java/com/example/jpademo/JpademoApplication.java
@@ -0,0 +1,48 @@
+package com.example.jpademo;
+
+import com.example.jpademo.entity.Product;
+import com.example.jpademo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpademoApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpademoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Product p1 = new Product("iPhone 16", "Smartphone", 16789.0);
+		System.out.println("Inserted: "+ productRepository.save(p1));
+
+		Product p2 = new Product("iPhone 15", "Smartphone", 15789.0);
+		System.out.println("Inserted: "+ productRepository.save(p2));
+
+		Product p3 = new Product("HP Laptop", "Laptop", 55666.9);
+		System.out.println("Inserted: "+ productRepository.save(p3));
+
+		System.out.println("All Products: "+productRepository.findAll());
+
+		System.out.println("Find By Category: "+ productRepository.findByCategory("Laptop"));
+
+		System.out.println("JPQL Result: "+ productRepository.findProductsMoreExpensiveThen(16000.0));
+
+		System.out.println("Native Query Result: "+ productRepository.searchByTitle("15"));
+		p2.setPrice(15555.0);
+		System.out.println("Updated: "+ productRepository.save(p2));
+
+		System.out.println("All Products: "+productRepository.findAll());
+
+		productRepository.deleteById(2);
+
+		System.out.println("All Products: "+productRepository.findAll());
+	}
+}
