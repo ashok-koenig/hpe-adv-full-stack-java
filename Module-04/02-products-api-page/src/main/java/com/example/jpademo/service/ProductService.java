1c95aec50b762e1a1d8f81679ebf6813935b2385
Ashok S <ashok.s@koenig-solutions.com>
2025-07-24 15:03:45 +0530

    cache demo

---
+++ Module-04/02-products-api-page/src/main/java/com/example/jpademo/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.example.jpademo.service;
+
+import com.example.jpademo.entity.Product;
+import com.example.jpademo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Cacheable(value = "products")
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    // Create and Update product
+    @CacheEvict(value = "products", allEntries = true)
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    @CacheEvict(value = "products", allEntries = true)
+    public Optional<Product> getProductById(Integer id){
+        return productRepository.findById(id);
+    }
+
+    @CacheEvict(value = "products", allEntries = true)
+    public void deleteProductById(Integer id){
+        productRepository.deleteById(id);
+    }
+}
