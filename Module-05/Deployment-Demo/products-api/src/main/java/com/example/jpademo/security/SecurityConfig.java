d5012eff9e67d63fc46c704c19b4da40fe75351a
Ashok S <ashok.s@koenig-solutions.com>
2025-07-25 15:59:04 +0530

    cors fix

--- Module-05/Deployment-Demo/products-api/src/main/java/com/example/jpademo/security/SecurityConfig.java
+++ Module-05/Deployment-Demo/products-api/src/main/java/com/example/jpademo/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost");
             }
         };
     }
